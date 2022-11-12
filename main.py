6aaf6f74ac6e8d42dda82788bda8ca85c63df86e
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2022-11-13 00:16:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,10 +57,12 @@ def updateplayer_fdata(name):
     if data["guild"]["name"]:
         player_fdata["guild"] = {"name": data["guild"]["name"],"rank": data["guild"]["rank"]}
         guildsdata[data["guild"]["name"]] = {"name": data["guild"]["name"]}
-    print(data["username"])
+    # print(data["username"])
     for character in data["characters"]:
         cdata = data["characters"][character]
-        # player_fdata["classes"][character] = {"type": cdata["type"], "blocksWalked": cdata["blocksWalked"], "playtime": cdata["playtime"]}
+        #min=i if i<j else j
+        a = cdata["blocksWalked"] if cdata["blocksWalked"]>0 else int(cdata["blocksWalked"])+(1 << 32)
+        player_fdata["classes"][character] = {"type": cdata["type"], "blocksWalked": a, "playtime": cdata["playtime"]}
         # print(player_fdata["classes"][character])
         for dungeon in data["characters"][character]["dungeons"]["list"]:
             try:
